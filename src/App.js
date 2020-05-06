eef90cd6b0e4885c4d217dc8291915a8d34d6143
Kent1 <gruberquentin67@gmail.com>
2020-05-06 17:58:47 +0200

    use charts

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React from "react";
 import BarChart from "./components/BarChart";
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <h1>COVID-19 Tracker</h1>
+class App extends React.Component {
+  render() {
+    const covid_data = require("./Covid-19-json.json");
+    const jsonQuery = require("json-query");
+    console.log(
+      jsonQuery("records[*geoId=DZ].cases", {
+        data: covid_data,
+      })
+    );
+
+    var fuck_dz = jsonQuery("records[*geoId=DZ].cases", {
+      data: covid_data,
+    });
+
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>COVID-19 Tracker</h1>
+        </div>
+        <div>
+          <BarChart data={[13, 14]} size={[500, 500]} />
+        </div>
       </div>
-      <div>
-        <BarChart data={[5, 10, 1, 3]} size={[500, 500]} />
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
